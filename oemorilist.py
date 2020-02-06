59915cfb9181c81d4b0df4a01f018b3b59768d08
Massacy <47631659+Massacy@users.noreply.github.com>
2020-02-07 03:45:01 +0900

    1st commit

---
+++ oemorilist.py
@@ -0,0 +1,50 @@
+import csv
+
+with open('easyodailist3.csv', 'r', encoding='shift_jis') as f:
+    reader = csv.reader(f)
+    tmp_list = []
+    for row in reader:
+        col = [x for x in row]
+        # print(l)
+        tmp_list.append(col)
+
+with open('easyodailist_ryo.csv', 'r', encoding='shift_jis') as g:
+    reader2 = csv.reader(g)
+    tmp_list2 = []
+    test = []
+
+    for row2 in reader2:
+        col2 = [x for x in row2]
+        tmp_list2.append(col2)
+        both = row2[0]
+        for i, y in enumerate(tmp_list):
+            if y[0] == both:
+                tmp_list[i][3] = row2[3]
+                test.append(tmp_list[i])
+
+    print("[")
+    for a in tmp_list:
+        print(f"\t{a},")
+    print("]")
+
+
+"""
+    for bb in reader2:
+        for j, yy in enumerate(test):
+            if bb[0] == yy[j]:
+                pass
+            else:
+                print(bb[0])
+"""
+
+"""    
+    for rowfin in tmp_list:
+        print(rowfin)
+
+print(counter)
+
+for aaa in test:
+    print(aaa)
+
+"""
+
